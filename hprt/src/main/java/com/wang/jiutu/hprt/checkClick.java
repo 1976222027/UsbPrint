ea5541670eb19977c663b68036e8e08359f71c25
1976222027@qq.com <2010MA520ma>
2019-11-27 17:01:07 +0800

    大幅度

---
+++ hprt/src/main/java/com/wang/jiutu/hprt/checkClick.java
@@ -0,0 +1,16 @@
+package com.wang.jiutu.hprt;
+
+public class checkClick
+{
+    private static final long CLICK_DELAY = 1000;
+    private static long mOldClickTime;
+    
+    public static boolean isClickEvent() 
+    {        
+    	long time = System.currentTimeMillis();
+    	if (time - mOldClickTime < CLICK_DELAY)     	      
+    		return false;
+    	mOldClickTime = time;
+    	return true;
+    }
+}
